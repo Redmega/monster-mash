e29310835a760cf7ec2ad0d20ef47fbc26971a00
Angel Piscola <angelpiscola@gmail.com>
2020-11-25 20:40:44 -0500

    feat: initial commit, nextjs

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans:
+        'Roboto, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      mono:
+        '"Roboto Mono", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      serif:
+        '"Modern Antiqua", ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+    },
+  },
+  variants: {
+    extend: {
+      textColor: ["visited"],
+    },
+  },
+  plugins: [],
+};
